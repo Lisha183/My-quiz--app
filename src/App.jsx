aacf3daef9661a2d60e65fb6609dbc0c263806f0
Alicia <chegealicia8@gmail.com>
2025-04-08 12:11:56 +0300

    Fix import statement for Login component: remove file extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login.jsx';
+import Login from './components/Login';
 
 
 function App() {
